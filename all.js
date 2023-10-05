861973bd8072aea0ac4884dd1f789f9f1ee65875
TzuruC <ostrich2821@gmail.com>
2023-10-05 19:01:05 +0800

    test url

--- all.js
+++ all.js
@@ -1,7 +1,7 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-const renderUrl = 'https://demo-yu09.onrender.com/vistas/';
+const renderUrl = 'https://54.191.253.12/vistas/';
 
 if (window.location.href.includes('index.html')) {
     //取得前台景點列表
